3a66a0b2cb7c688b18e36088baf12643373f7c75
Yves Hoebeke <yves.hoebeke@gmail.com>
2017-10-09 12:26:53 -0400

    check data change detection

--- src/logusage/logusage.go
+++ src/logusage/logusage.go
@@ -4,36 +4,70 @@ package logusage
 import (
   "fmt"
   "os"
+  "io"
   "time"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
   "myconfig"
+  "bytes"
+  "encoding/hex"
+  "crypto/md5"
 )
 
 // sesson data record
 type Runtimeinfo struct {
         User string
+        Navhash string
         Runtime string
+}
+
+// get MD5 checksum of some file
+func getMD5(filename string) string {
+  file, err := os.Open(filename)
+  if err != nil {
+    panic(err)
+  }
+  defer file.Close()
+
+  hash := md5.New()
+  if _, err := io.Copy(hash, file); err != nil {
+    panic(err)
+  }
+
+  decoded := hex.EncodeToString(hash.Sum(nil))
+  if err != nil {
+    panic(err)
+  }
+
+  return decoded
 }
 
 // record current session
 func Logit() {
   config := myconfig.Getconfig()
+  var buffer bytes.Buffer
+  var navplanfilename string
+  // connect to mongo db
   session, err := mgo.Dial(config.Database.Host)
   if err != nil {
           panic(err)
   }
   defer session.Close()
-
   // Optional. Switching to monotonic session behavior. Data volume
   // very low here, no impact but leave it in as a matter of principle.
   session.SetMode(mgo.Monotonic, true)
+  // reference the collection
   c := session.DB(config.Database.Databasename).C(config.Database.Collectionname)
-
+  // construct the navplan data file name to get hash, so we can see what
+  // the user was operating with.
+  buffer.WriteString(os.Getenv("GOPATH"))
+  buffer.WriteString(config.Navdata.Navplan)
+  navplanfilename  = buffer.String()
+  // get the current date time in UTC
   loc, _ := time.LoadLocation("UTC")
   now := time.Now().In(loc)
-
-  if err = c.Insert(&Runtimeinfo{os.Getenv("USER"), now.String()}); err != nil {
+  // record it
+  if err = c.Insert(&Runtimeinfo{os.Getenv("USER"), getMD5(navplanfilename), now.String()}); err != nil {
     fmt.Println(err.Error())
     os.Exit(1)
   }
@@ -47,22 +81,30 @@ func Showall(userlogin string) {
     panic(err)
   }
   defer session.Close()
-
   // Optional. Switching to monotonic session behavior. Data volume
   // very low here, no impact but leave it in as a matter of principle.
   session.SetMode(mgo.Monotonic, true)
   c := session.DB(config.Database.Databasename).C(config.Database.Collectionname)
 
+  // yeah, I could've definded the array in the beginning. left over
+  // from my original version
   results := []Runtimeinfo{}
-
+  // find all access records for this user and display to console
   if err = c.Find(bson.M{"user": userlogin}).All(&results); err != nil {
     panic(err)
   } else {
-      if len(results) == 0 {
-        fmt.Println("No previous access.")
-      } else {}
-        for i := range results {
-          fmt.Printf("%d.\t%s\n",i+1,results[i].Runtime)
+    var hashchangeflag string
+    if len(results) == 0 {
+      fmt.Println("No previous access found.")
+    } else {
+      for i := range results {
+        if i > 0 && results[i].Navhash != results[i-1].Navhash {
+          hashchangeflag = "!"
+        } else {
+          hashchangeflag = " "
+        }
+        fmt.Printf("%d.\t%s\t%s %s\n", i+1, results[i].Runtime, results[i].Navhash, hashchangeflag)
       }
+    }
   }
 }
